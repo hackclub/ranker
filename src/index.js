518adbaafc1e683d7db6d2369e6c14eab399b421
bajpai244 <bajpaiharsh244@gmail.com>
2021-08-15 17:19:15 +0530

    feat: add message config for who-am-i command

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const { App } = require("@slack/bolt");
 const { WebClient, LogLevel } = require("@slack/web-api");
 
+const { who_am_i } = require("../lib/utils/index");
+
 const oauth_token = process.env.OAUTH_TOKEN;
 const app_token = process.env.APP_TOKEN;
 
@@ -23,8 +25,4 @@ const app = new App({
   console.log("⚡️ Bolt app started");
 })();
 
-app.command("/authenticate", async ({ command, ack, respond, context }) => {
-  await ack();
-
-  const { user_id, channel_id, channel_name } = command;
-});
+app.command("/who-am-i", who_am_i);
