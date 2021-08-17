0b5ea5739155b4a49242c930feaa2e73522f4468
bajpai244 <bajpaiharsh244@gmail.com>
2021-08-17 14:06:22 +0530

    feat(cover_image): complete cover_image module

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ require("dotenv").config();
 const { App } = require("@slack/bolt");
 const { WebClient, LogLevel } = require("@slack/web-api");
 
-const { who_am_i, change_logo, change_desc } = require("../lib/commands/index");
+const {
+  who_am_i,
+  change_logo,
+  change_desc,
+  change_cover,
+} = require("../lib/commands/index");
 const {
   change_logo_view_1,
   change_desc_view_1,
+  change_cover_view_1,
 } = require("../lib/events/index");
 
 const oauth_token = process.env.OAUTH_TOKEN;
@@ -32,6 +38,8 @@ const app = new App({
 app.command("/who-am-i", who_am_i);
 app.command("/change-logo", change_logo);
 app.command("/change-description", change_desc);
+app.command("/change-cover", change_cover);
 
 app.view("change_logo_view_1", change_logo_view_1);
 app.view("change_desc_view_1", change_desc_view_1);
+app.view("change_cover_view_1", change_cover_view_1);
